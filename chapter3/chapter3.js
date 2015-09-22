c188d4ec92b8ae55a58669d78e066e74b784e016
Sean Amog <sj.amog@gmail.com>
2015-09-21 21:46:52 -0400

    semicolon

--- chapter3/chapter3.js
+++ chapter3/chapter3.js
@@ -1,22 +1,13 @@
 //set main namespace
 goog.provide('chapter3');
 
-
 //get requirements
 goog.require('lime.Director');
-goog.require('lime.Scene');
-goog.require('lime.Layer');
-goog.require('lime.Circle');
-goog.require('lime.Label');
-goog.require('lime.animation.Spawn');
-goog.require('lime.animation.FadeTo');
-goog.require('lime.animation.ScaleTo');
-goog.require('lime.animation.MoveTo');
-
 
 // entrypoint
 chapter3.start = function(){
     
+    var director = new lime.Director(document.bodym800,640);
 
 }
 
