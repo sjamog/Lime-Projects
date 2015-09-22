646799466b234ef0b9ad4d6c852a39ada107fcff
Sean Amog <sj.amog@gmail.com>
2015-09-21 21:40:31 -0400

    add chapters

---
+++ chapter3/chapter3.js
@@ -0,0 +1,25 @@
+//set main namespace
+goog.provide('chapter3');
+
+
+//get requirements
+goog.require('lime.Director');
+goog.require('lime.Scene');
+goog.require('lime.Layer');
+goog.require('lime.Circle');
+goog.require('lime.Label');
+goog.require('lime.animation.Spawn');
+goog.require('lime.animation.FadeTo');
+goog.require('lime.animation.ScaleTo');
+goog.require('lime.animation.MoveTo');
+
+
+// entrypoint
+chapter3.start = function(){
+    
+
+}
+
+
+//this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
+goog.exportSymbol('chapter3.start', chapter3.start);
